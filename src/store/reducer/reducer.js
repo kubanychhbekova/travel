689685254e83b4b33bef1d7e5c6f9db027cc6358
kanjar45 <kenovkenn@gmail.com>
2023-07-15 13:08:03 +0600

    popular

--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -1,12 +1,18 @@
 const initialState = {
+    popular:[],
     product: [],
+    cards:[]
 };
 
 export const Reducer = (state = initialState, action) => {
     switch (action.type) {
         case "GET_PRODUCT":
             return { ...state, product: action.payload };
-
+        case "GET_POPULAR":
+            return { ...state, popular: action.payload };
+        case "GET_CARDS":
+            const found = state.popular.find((el) => el.id === action.payload);
+            return { ...state, cards:[ { ...found}]};
         default:
             return state;
     }
